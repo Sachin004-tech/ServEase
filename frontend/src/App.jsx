da5c5b249d8320db3a58832116cb621e175c6696
Sachin <gautampawar7562@gmail.com>
2025-09-07 18:17:37 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Demo from "./components/Demo";
+
+function App() {
+  return (
+    <div>
+      <h1>React + Flask Demo</h1>
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
