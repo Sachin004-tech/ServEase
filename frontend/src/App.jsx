56261da269c7971f37f5c668446f1e4c5a38734d
Sachin <gautampawar7562@gmail.com>
2025-10-01 21:11:55 +0530

    add home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import LoginPage from "./pages/LoginPage";
 import Navbar from "./layouts/Navbar";
 import SignUpCustomer from "./pages/SignUpCustomer";
 import SignUpProfessional from "./pages/SignUpProfessional";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return <>
     <Navbar/>
    <Routes>
+    <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/signupcustomer" element={<SignUpCustomer />} />
     <Route path="/signupprofessional" element={<SignUpProfessional />} />
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to ServEase</h1>
+      <p className="text-lg mt-4">Your one-stop platform for services.</p>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
